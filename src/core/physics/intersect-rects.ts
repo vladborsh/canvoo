fa748272c2673fe32c3f877bc6d181538a78abbd
Borsh Vladislav Aleksandrovich <ext.vborsh@tcsbank.ru>
2022-05-01 22:52:33 +0300

    fix rect intersection

--- src/core/physics/intersect-rects.ts
+++ src/core/physics/intersect-rects.ts
@@ -1,16 +1,4 @@
-import { Vector } from '../interfaces/vector';
-import { Direction } from '../interfaces/direction';
-import { multiply, sum } from '../utils/calc';
 import { Collider } from '../interfaces/collider';
-
-export const FREE_ACCELERATION: Vector = { x: 0, y: 80 };
-
-export type RectCollision = Record<Direction, Collision>;
-
-interface Collision {
-  isCollided: boolean;
-  coordinate?: number;
-}
 
 /*
  https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
@@ -19,10 +7,19 @@ export function intersectRects(
   collider1: Collider,
   collider2: Collider,
 ): boolean {
+  const coll1minX = collider1.position.x - collider1.size.x / 2;
+  const coll1maxX = collider1.position.x + collider1.size.x / 2;
+  const coll1minY = collider1.position.y - collider1.size.y / 2;
+  const coll1maxY = collider1.position.y + collider1.size.y / 2;
+  const coll2minX = collider2.position.x - collider2.size.x / 2;
+  const coll2maxX = collider2.position.x + collider2.size.x / 2;
+  const coll2minY = collider2.position.y - collider2.size.y / 2;
+  const coll2maxY = collider2.position.y + collider2.size.y / 2;
+
   return (
-    collider1.position.x < collider2.position.x + collider2.size.x &&
-    collider1.position.x + collider1.size.x > collider2.position.x &&
-    collider1.position.y < collider2.position.y + collider2.size.y &&
-    collider1.position.y + collider1.size.y > collider2.position.y
+    coll1minX <= coll2maxX &&
+    coll1maxX >= coll2minX &&
+    coll1minY <= coll2maxY &&
+    coll1maxY > coll2minY
   );
 }
